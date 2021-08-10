b6a2de74b3afa32301036a75187be6669ad09652
Chillieman <chillieman16@gmail.com>
2021-08-10 19:52:46 -0400

    Cleaning up spelling warnings

--- app/src/main/java/com/chillieman/chilliewallet/di/annotation/FragmentScoped.kt
+++ app/src/main/java/com/chillieman/chilliewallet/di/annotation/FragmentScoped.kt
@@ -3,7 +3,7 @@ package com.chillieman.chilliewallet.di.annotation
 import javax.inject.Scope
 
 /**
- * In Dagger, an unscoped component cannot depend on a scoped component. As the application
+ * In Dagger, an un-scoped component cannot depend on a scoped component. As the application
  * [dagger.Component] is a scoped component (`@Singleton`, we create a custom
  * scope to be used by all fragment components. Additionally, a component with a specific scope
  * cannot have a sub component with the same scope.
